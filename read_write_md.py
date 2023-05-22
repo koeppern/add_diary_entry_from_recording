73a07cde680a6eded0d2b095df971aff881823ca
Johannes Koeppern <johannes.koeppern@gmail.com>
2023-05-22 11:16:11 +0200

    functions refactores

--- read_write_md.py
+++ read_write_md.py
@@ -5,40 +5,49 @@ import pandas as pd
 import os
 
 
+# Parameters
 filename = "G:\Meine Ablage\johannes_notes\diary_johannes_koeppern.md"
 
 filename_out = "out.md"
 
+# Funcitons
+def write_df_to_md_file(filename_out, df):
+    dates_texts = df.groupby("date").apply(lambda x: "\n".join("- " + row["text"] for _, row in x.iterrows()) + "\n").reset_index()
+    dates_texts["date"] = pd.to_datetime(dates_texts["date"])  # Convert date column to datetime type
+    dates_texts = dates_texts.sort_values("date")  # Sort by date column in ascending order
+    dates_texts["date"] = dates_texts["date"].dt.strftime("%Y-%m-%d")  # Convert date column back to string format
 
+    texts_out = "# Diary\n\n" + "\n".join(["## " + row["date"] + "\n" + row[0] for _, row in dates_texts.iterrows()])
+
+    with open(filename_out, "w") as file:
+        file.write(texts_out)
+
+def load_md_file_into_df(filename, df):
+    with open(filename, "r") as file:
+        text_in = file.read()
+
+    lines = text_in.split("\n")
+
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith("## "):
+                this_date = line[3:]
+        elif line.startswith("- "):
+            this_text = line[2:].lstrip(" ")
+
+
+            new_row = pd.DataFrame({"date": [this_date], "text": [this_text]})
+
+            df = pd.concat([df, new_row], ignore_index=True)
+    return df
+
+# Main script
 df = pd.DataFrame()
 
-with open(filename, "r") as file:
-    text_in = file.read()
+df = load_md_file_into_df(filename, df)
 
-lines = text_in.split("\n")
-
-for line in lines:
-    line = line.strip()
-
-    if line.startswith("## "):
-            this_date = line[3:]
-    elif line.startswith("- "):
-        this_text = line[2:].lstrip(" ")
-
-
-        new_row = pd.DataFrame({"date": [this_date], "text": [this_text]})
-
-        df = pd.concat([df, new_row], ignore_index=True)
-
-dates_texts = df.groupby("date").apply(lambda x: "\n".join("- " + row["text"] for _, row in x.iterrows()) + "\n").reset_index()
-dates_texts["date"] = pd.to_datetime(dates_texts["date"])  # Convert date column to datetime type
-dates_texts = dates_texts.sort_values("date")  # Sort by date column in ascending order
-dates_texts["date"] = dates_texts["date"].dt.strftime("%Y-%m-%d")  # Convert date column back to string format
-
-texts_out = "# Diary\n\n" + "\n".join(["## " + row["date"] + "\n" + row[0] for _, row in dates_texts.iterrows()])
-
-with open(filename_out, "w") as file:
-    file.write(texts_out)
+write_df_to_md_file(filename_out, df)
 
 
 
